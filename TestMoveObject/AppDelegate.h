3092d52061dd55c59c9e209e52a7a584e7b0e0e7
Do tran <ankiinn@gmail.com>
2016-05-22 17:28:04 +0700

    Initial Commit

---
+++ TestMoveObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMoveObject
+//
+//  Created by Do tran on 5/22/16.
+//  Copyright © 2016 Do tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestMoveObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMoveObject
+//
+//  Created by Do tran on 5/22/16.
+//  Copyright © 2016 Do tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
